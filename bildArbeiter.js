3dd6ad51b1eb0664983b379468f420460a943341
Michtz <105941997+Michtz@users.noreply.github.com>
2022-09-09 16:54:29 +0200

    Add files via upload

---
+++ bildArbeiter.js
@@ -0,0 +1,26 @@
+
+
+
+
+
+
+
+    // Bild von Julius Meinhof
+    let srcBildJulius = "bildJulius.jpg";
+
+                
+    let srcBildStuhl = "stuhlDesign.jpg";
+
+
+    let srcLogoVideolution = "https://usercontent.one/wp/www.meinhof.ch/wp-content/uploads/2022/07/videolutions-logo_white.png?media=1653409358&quot";
+    let srcLogoSchreinereiMeer = "https://usercontent.one/wp/www.meinhof.ch/wp-content/uploads/2022/07/1477141671-e1658220760603.png?media=1653409358&quot";
+    let srcLogoZentrumEichgut = "https://usercontent.one/wp/www.meinhof.ch/wp-content/uploads/2022/07/logo_op-zentrum_eichgut.png?media=1653409358&quot";
+
+
+
+
+        function bildJulius() {
+                document.getElementById("bildJulius").setAttribute("src", srcBildJulius )
+        }
+
+
